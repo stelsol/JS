e26e6292f6e8ade4211173b579f043e76e76ea8f
stelsol <132797421+stelsol@users.noreply.github.com>
2025-01-21 23:29:06 -0300

    Aprendendo JS
    Estou me aprofundando mais na linguagem

---
+++ Udemy/aula7/index.js
@@ -0,0 +1,7 @@
+// + - * /
+const primeiroNumero = '10' //String
+const segundoNumero = 20 //Number 
+const soma = primeiroNumero + segundoNumero
+console.log(soma)
+console.log('O tipo do primeiro número é :', typeof primeiroNumero)
+console.log('O tipo do segundo numero é :', typeof segundoNumero)
---
+++ Udemy/aula8/exercicio.js
@@ -0,0 +1,10 @@
+const nome = 'Stella'
+const lastName = ' Rocha'
+let age = 20
+let height = 1.69
+let weight = 65
+let calc // podemos reservar a var e fazer o calculo em baixo
+calc = weight / ( height * height)
+let anonasc = 2025 - age
+console.log(nome + lastName, 'She is have',age,'years old',`weight:${weight} kg height:`,height,'and your IMC is: ',calc,'she was born in',anonasc)
+//console.log(anonasc) - Usei para testar se estava certo!

225910db9053160234f38a86c8e6fe48b537876c
stelsol <132797421+stelsol@users.noreply.github.com>
2025-01-25 23:22:28 -0300

    Aprendendo operadores Aritméticos

---
+++ Udemy/aula11/index.js
@@ -0,0 +1,27 @@
+let contador = 0
+for(let c = contador ; c <= 10 ; c++){
+    console.log(c)
+}
+
+
+
+/*
+const num1 = 3
+const num2 = 4
+console.log(num1 % num2)
+*/
+/* Converte para numero inteiro e apenas inteiro
+const num1 = 3
+const num2 = parseInt('7')
+*/
+/* Converte numero com . diferente do anterior que não considera o .
+console.log(num1 % num2)
+const num1 = 3
+const num2 = parseFloat('3.3')
+console.log(num1 + num2)
+*/
+/* converte ambos os tipos inteiro e com . 
+const num1 = 3
+const num2 = Number('50.9')
+console.log(num1 + num2)
+*/

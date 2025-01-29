f3feef9dd09555301493a90cc45d01e134e15764
stelsol <132797421+stelsol@users.noreply.github.com>
2025-01-29 00:21:21 -0300

    Update number.js

--- Udemy/aula14/number.js
+++ Udemy/aula14/number.js
@@ -16,8 +16,8 @@ console.log(Number.isInteger(num1))
 */
 let num1 = 0.7
 let num2 = 0.1
-num1 += num2
-num1 += num2
-num1 += num2
+num1 = ((num1 * 100) + (num2 * 100) ) /100
+num1 = ((num1 * 100) + (num2 * 100) ) /100
+num1 = ((num1 * 100) + (num2 * 100) ) /100
 console.log(num1)
 console.log(Number.isInteger(num1))

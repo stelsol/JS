218623d10aba11eba542864938688c6ec78b95c3
stelsol <132797421+stelsol@users.noreply.github.com>
2025-01-31 22:30:26 -0300

    arrays

---
+++ Udemy/aula16/index.js
@@ -0,0 +1,15 @@
+let alunos = ['Stella','Carla','joana']
+console.log(alunos) // Lista inicial
+//let removido = alunos.pop() - Remove do final da lista
+//let removido = alunos.shift() - Remove do inicio
+//alunos.unshift('Carol') - adiciona ao inicio da lista
+/*
+alunos.push('Kaique') // Adiciona ao final da lista
+console.log(alunos.length)// Mostra quantos indices tem
+console.log(alunos) // Lista após as edições
+console.log(alunos[2]) //Exibe o indice 2
+alunos[0] = 'Luana' // Altera o elemento dentro do indice informado
+console.log(alunos)
+alunos[3] = 'Karen' // Adicionando um elemento ao indice informado, se não houver ele cria
+console.log(alunos)
+*/

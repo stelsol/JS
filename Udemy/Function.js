a460dc7890ea96364be9bf4ac083d4d3d52a1ad7
stelsol <132797421+stelsol@users.noreply.github.com>
2025-02-02 23:18:41 -0300

    Functions Básicos

---
+++ Udemy/Function.js
@@ -0,0 +1,44 @@
+/* Simplificação da arofunction 
+const raiz = n => n ** 0.5
+console.log(raiz(9))
+*/
+/* const raiz = (n) =>{
+    return n ** 0.5
+}; // function arofunction
+console.log(raiz(9))
+console.log(raiz(60))
+console.log(raiz(20))
+*/
+
+/* const raiz = function(n){
+    return n ** 0.5
+};
+console.log(raiz(9))
+console.log(raiz(60))
+console.log(raiz(20))
+*/
+/*function soma(x = 1, y = 1){
+    const resultado = x + y // Não conseguimos ter acesso e nem  modificar nada por fora
+    return resultado
+}
+const resultado = soma() // Podemos redeclarar pois a coonst dentro da function, está protegida la dentro
+console.log(resultado)
+*/
+
+/*
+function soma(x, y){
+    const resultado = x + y
+    return resultado
+}
+console.log(soma(13, 1))
+console.log(soma(2, 9))
+
+
+
+ Fução Basíca!!
+function Saudacao(nome){
+    return`Olá Mundo ${nome}`
+}
+const variavel = Saudacao('Stella')
+console.log(variavel)
+*/

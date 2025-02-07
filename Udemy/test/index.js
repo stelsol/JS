d35f47fa3c9f5b84307e3dd271579d5cdfe0382a
stelsol <132797421+stelsol@users.noreply.github.com>
2025-02-06 22:33:13 -0300

    Comecei o js do exercício

--- Udemy/test/index.js
+++ Udemy/test/index.js
@@ -1,3 +1,19 @@
 function Exibir(){
-    
+ const nome = document.querySelector('#Nome')
+ const snome = document.querySelector('#Snome')
+ const peso = document.querySelector('#Peso')
+ const altura = document.querySelector('#Alt')
+ 
+const pessoas = []
+ pessoas.push({
+    nome:nome.value,
+    snome:snome.value,
+    peso:peso.value,
+    altura:altura.value
+ })
+ 
+ let res = document.querySelector('#res')
+ res.innerHTML += `<p>Nome : ${nome.value} ${snome.value} Peso: ${peso.value} Altura:  ${altura.value}</p>`
+ 
+ console.log(`${nome.value} ${snome.value} ${peso.value} ${altura.value}`)
 }

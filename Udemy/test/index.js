b1d57430307fa72be6421a5197714246579f7010
stelsol <132797421+stelsol@users.noreply.github.com>
2025-02-10 21:35:45 -0300

    Operadores lógicos
    fiz o exercício e resolvi entender ele melhor, logo após apendi sobre operadores lógicos.

--- Udemy/test/index.js
+++ Udemy/test/index.js
@@ -1,3 +1,5 @@
+// Preciso melhorar !
+
 function Exibir(){
  const nome = document.querySelector('#Nome')
  const snome = document.querySelector('#Snome')
@@ -15,5 +17,39 @@ const pessoas = []
  let res = document.querySelector('#res')
  res.innerHTML += `<p>Nome : ${nome.value} ${snome.value} Peso: ${peso.value} Altura:  ${altura.value}</p>`
  
- console.log(`${nome.value} ${snome.value} ${peso.value} ${altura.value}`)
+ //console.log(`${nome.value} ${snome.value} ${peso.value} ${altura.value}`)
+
 }
+
+/* Fazendo como ele fez: lembrando ele não utilizou css e nem onclick;
+ele quer que a pagina não atualize. 
+
+O jeito dele bem melhor e segue as boas praticas
+
+function MeuEscopo(){
+   const form = document.querySelector('#form')
+   const res = document.querySelector('#res')
+
+   const Pessoas = []
+   function RecebeEventoSubmit (evento){
+      evento.preventdefault()
+      evento.preventdefault()
+      const nome = form.querySelector('#nome')
+      const Snome = form.querySelector('#Snome')
+      const Peso = form.querySelector('#Peso')
+      const Alt = form.querySelector('#Altura')
+
+      pessoas.push({
+         nome:nome.value,
+         Snome:Snome.value,
+         Peso:Peso.value,
+         Alt:Alt.value
+      })
+      
+      res.inner += `${nome.value} ${Snome.value} ${Peso.value} ${Alt.value}`
+   }
+
+   form.addEventListener('Submit', RecebeEventoSubmit)
+}
+   MeuEscopo()
+*/

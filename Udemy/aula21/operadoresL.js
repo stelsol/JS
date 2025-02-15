28d92e3efcc7eed1b76bfbe5bd56e64b3d7e48f6
stelsol <132797421+stelsol@users.noreply.github.com>
2025-02-14 22:52:08 -0300

    Operadore e ShortC

---
+++ Udemy/aula21/operadoresL.js
@@ -0,0 +1,6 @@
+/* Operadores Lógicos 
+
+&& -> AND -> E -- Todas as Expressões precisam ser verdadeiras para retornar TRUE
+|| -> Or -> Ou
+! -> Not -> Não 
+*/
---
+++ Udemy/aula22/short-circuit.js
@@ -0,0 +1,10 @@
+function MensagemOla(){
+    console.log('Olá Mundo!')
+}
+const executarMensagem = false
+
+executarMensagem && MensagemOla()
+
+/*const color = "White"
+const corEscolhida = color || 'Dark'
+console.log(corEscolhida)*/

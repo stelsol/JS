53a86bd43f2727c836b24b88830600b0692bd26e
stelsol <132797421+stelsol@users.noreply.github.com>
2025-03-26 22:06:58 -0300

    Exercicio date e switch

---
+++ Udemy/aula30/data.js
@@ -0,0 +1,72 @@
+function zeroaesquerda(num){
+    return num >= 10 ? num : `0${num}` 
+}
+function hoje(horario){
+    const dia = zeroaesquerda(horario.getDate())
+   //const mes = zeroaesquerda(horario.getMonth()+1)
+    const ano = zeroaesquerda(horario.getFullYear())
+    const hr = zeroaesquerda(horario.getHours())
+    const min = zeroaesquerda(horario.getMinutes())
+    return `${semana(semanaconv)} - ${dia} de ${mesdescrito(Convert)} de ${ano} ${hr}:${min}`
+    
+}
+function mesdescrito(Convert){
+    switch (Convert) {
+        case 0:
+        return 'Janeiro'
+        case 1:
+        return 'Fevereiro'
+        case 2:
+        return 'Março'
+        case 3:
+        return 'Abril'
+        case 4:
+        return 'Maio'
+        case 5:
+        return 'Junho'
+        case 6:
+        return 'Julho'
+        case 7:
+        return 'Agosto'
+        case 8:
+        return 'Setembro'
+        case 9:
+        return 'Outubro'
+        case 10:
+        return 'Novembro'
+        case 11:
+        return 'Dezembro'
+        default:
+        return  ' '
+    }
+}
+
+function semana(semanaconv){
+    switch (semanaconv) {
+        case 0:
+            return 'Domingo'
+        case 1:
+            return 'Seguda-Feira'
+        case 2:
+            return 'Terça-Feira'
+        case 3:
+            return 'Quarta-Feira'
+        case 4:
+            return 'Quinta-Feira'
+        case 5:
+            return 'Sexta-Feira'
+        case 6:
+            return 'Sábado'
+    
+        default:
+            return ' '
+    }
+}
+
+const horario = new Date()
+const Convert = horario.getMonth()
+const semanaconv = horario.getDay()
+const exibir = document.querySelector('#horadia')
+//console.log(hoje(horario))
+
+exibir.innerHTML = `${hoje(horario)}`

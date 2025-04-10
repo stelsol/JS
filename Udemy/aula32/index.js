416106dbe4cf6672235a5c0ed884551a3d60c39e
stelsol <132797421+stelsol@users.noreply.github.com>
2025-04-09 23:45:43 -0300

    Desestruturacao arrays

--- Udemy/aula32/index.js
+++ Udemy/aula32/index.js
@@ -1,3 +1,35 @@
+/*//! Aprendendo desestruturação de arrays
+
+let presentes = ['Bola','Boneca','avião']
+let [present1, present2,present3] = presentes
+
+
+//! Ignorando alguma informação que não quisermos
+
+let lanches = ['Brigadeiro', 'Pizza', 'Sanduiche']
+let [lanche1, lanche2, lanche3] =lanches
+
+console.log(lanche1, lanche3)
+//!Pegando o resto 
+let numeros = [10, 20, 30, 40, 50, 60, 70, 80,]
+let [primeiro, segundo, terceiro, ...rest] = numeros
+
+console.log(rest)
+console.log(present3, present1)
+//!                  0             1             2
+//?             0    1   2     0   1   2    0    1   2
+let numeros = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
+console.log(numeros [1][1]) // vai exibir 50, pois ele pegou o array com indice 1 e dentro tinha o outro array e continha um indeci 1 que estava guardando o numero 50
+*/
+//* maneira mais complexa 
+//!                  0             1             2
+//?             0    1   2     0   1   2    0    1   2
+let numeros = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
+let [,,[,,noventa]] = numeros
+console.log(noventa)
+
+
+
 // let a = 'A';
 // let b = 'B';
 // let c = 'C';
@@ -6,7 +38,7 @@
 // [a,b,c] = letras;
 // console.log(a,b,c);
 
-const furtas = ['bananas','uva','morango']
-[uva,morango,bananas] = furtas
+// const furtas = ['bananas','uva','morango']
+// [uva,morango,bananas] = furtas
 
-console.log(uva)
+// console.log(uva)

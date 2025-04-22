0901c1f885115dc73a616a3fecf47071d71eb495
stelsol <132797421+stelsol@users.noreply.github.com>
2025-04-21 21:19:33 -0300

    Revendo desestruturação

--- Udemy/aula33/dobjetos.js
+++ Udemy/aula33/dobjetos.js
@@ -4,15 +4,28 @@
 
 */
 
-const pessoa = {
-    nome:'Kauã',
-    idade: 21,
-    cidade:'Taubaté',
-    endereco: {
-        rua: 'r.Domingos Lofuto',
-        numero:130
+// const pessoa = {
+//     nome:'Kauã',
+//     idade: 21,
+//     cidade:'Taubaté',
+//     endereco: {
+//         rua: 'r.Domingos Lofuto',
+//         numero:130
+//     }
+// }
+// const {nome: n,idade,cidade, endereco:{rua,numero}, endereco} = pessoa
+
+// console.log(endereco)
+
+const frutas = {
+    f1:'Banana',
+    f2:'Uva',
+    f3:'Morango',
+    calda:{
+        c1:'Chocolate',
+        c2:'Chocolate Branco'
     }
 }
-const {nome: n,idade,cidade, endereco:{rua,numero}, endereco} = pessoa
-
-console.log(endereco)
+const {f3,calda:{c1,c2}}=frutas
+// Via desestruturação é para desestruturar e pegar informações exatas que seja necessaria
+console.log(f3,c2) 

8b0d8133fd2eeb353811f2d2ca5055fc1633e6cc
stelsol <132797421+stelsol@users.noreply.github.com>
2025-05-05 20:16:10 -0300

    Aprendendo mais sobre o for

--- Udemy/aula34/for.js
+++ Udemy/aula34/for.js
@@ -4,7 +4,20 @@
 //     console.log(`bloco ${i}`)
 // }
 
-// ! Para funcionar adicionando utilizamos o sinalde <
+// ! Para funcionar adicionando utilizamos o sinal de <
 // for (let i = 0; i <= 10; i++){
 //     console.log(`bloco ${i}`)
 // }
+//! Para Taubuada 
+    // for(let i = 0 ; i <= 10; i++){
+    //     let tabuada = 7
+    //     let mult = tabuada * i 
+    //     console.log(`${tabuada} X ${i} = ${mult}`)
+    // }
+
+//! Para percorrer arrays 
+const frutas = ['Morango', 'Uva', 'Abacate', 'Manga', 'Stella','carro']
+
+for( let i = 0; i < frutas.length; i++){
+    console.log(`${i}`,frutas[i])
+}

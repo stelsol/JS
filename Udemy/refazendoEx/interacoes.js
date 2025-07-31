55032565fa79fc45d9c0f1989e59be3857cc3b0f
stelsol <132797421+stelsol@users.noreply.github.com>
2025-07-31 20:17:29 -0300

    adicionando  js

---
+++ Udemy/refazendoEx/interacoes.js
@@ -0,0 +1,10 @@
+const conteudo = [
+    {tag:'img', src:'imagens/sapoprincessa.webp', alt:'A princesa e o Sapo', classe:'ilus-img'},
+    {tag:'p', texto:'Tiana é uma jovem determinada que sonha em abrir seu próprio restaurante em Nova Orleans. Sua vida muda quando conhece um príncipe transformado em sapo por um feitiço. Ao beijá-lo, ela também se transforma e os dois embarcam numa jornada pelo pântano para quebrar a maldição. No caminho, enfrentam perigos, fazem novos amigos e descobrem o verdadeiro significado do amor. Uma história mágica sobre sonhos, coragem e escolhas do coração.', classe:'ilus-txt'}
+]
+const exibir = document.querySelector('.container')
+const div = document.createElement('div')
+
+for (let i = 0; i = conteudo.length; i++){
+    
+}

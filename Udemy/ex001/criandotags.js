88fd9877ad4f318eb71e85d5ddf7ac9f72d97139
stelsol <132797421+stelsol@users.noreply.github.com>
2025-08-19 20:48:56 -0300

    continuando o desenvolvimento

--- Udemy/ex001/criandotags.js
+++ Udemy/ex001/criandotags.js
@@ -1,7 +1,7 @@
 
 function criandotag(){
     let container1 = document.querySelector('.main-container')
-    let div1 = document.createElement('div')
+    let div2 = document.createElement('div')
     const conteudo = [
         {tag:'p',texto:'Em um palácio imperial cheio de intrigas, Maomao, uma jovem farmacêutica astuta e observadora, é sequestrada e vendida como serva no harém do imperador. Usando seu conhecimento de venenos, medicamentos e uma sagacidade afiada, ela resolve mistérios médicos e casos envenenamentos, chamando a atenção do eunuco-chefe Jinshi.', classe:'cartas'}
     ]
@@ -10,14 +10,25 @@ function criandotag(){
         let {tag,texto,classe} = conteudo[i]
         let tagcriada = document.createElement(tag)
         tagcriada.innerText = texto
-        div1.appendChild(tagcriada)
+        div2.appendChild(tagcriada)
         if(classe){
             tagcriada.classList.add(classe)
         }
     }
-    container1.appendChild(div1)
-    return div1
+    container1.appendChild(div2)
+    return div2
 }
 criandotag()
+function criandodiv2(){
+    let conteudo2  = document.querySelector('.main-container')
+    let div1 = document.createElement('div')
 
+    const tag2 = [
+        {tag:'img', src:''}
+    ]
+
+    for(let i = 0; i < conteudo2.length; i++){
+
+    }
+}
 //tivemos erros, digitamos alguns elementos errados! e nos perdemos um pouco na lógica do return da function

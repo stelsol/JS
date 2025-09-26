cf877792e3c1cbcc2f35c8ade7ab71dd5feb4c8c
stelsol <132797421+stelsol@users.noreply.github.com>
2025-09-25 21:38:37 -0300

    Começando for of

--- Udemy/aula36/exfinal.js
+++ Udemy/aula36/exfinal.js
@@ -5,6 +5,8 @@ let usuario = {
     cidade: "São Paulo"
 }
 for(let informacoes in usuario){
-    // console.log(`${informacoes} -> ${usuario[informacoes]}`)
-    console.log(`O usuário ${usuario.nome} tem ${usuario.idade}, atua na area de ${usuario.profissao} e vive em ${usuario.cidade}`)
+    console.log(`${informacoes} -> ${usuario[informacoes]}`)
+    
 }
+// a parte da frase não precisa ficar dentro do laço pois o desafio era pegar o dado contido dentro do objeto e exibilo fora
+console.log(`O usuário ${usuario.nome} tem ${usuario.idade}, atua na area de ${usuario.profissao} e vive em ${usuario.cidade}`)

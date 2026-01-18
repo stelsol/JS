076191adf7e1e285e4fcc68c4f46b0b7565f8d6f
stelsol <132797421+stelsol@users.noreply.github.com>
2026-01-18 11:50:14 -0300

    Finalizando exercicio

--- testando/ex1/test.js
+++ testando/ex1/test.js
@@ -1,12 +1,14 @@
 
-let todo = document.querySelector('.container')
-let nome = todo.querySelector('#nome').value
-todo.addEventListener('submit', function criarp(){  
-        let p = document.createElement('p')
-        let exibindo = document.getElementById('resultado')
+// let todo = document.querySelector('.container')
+// let nome = todo.querySelector('#nome').value
+// todo.addEventListener('submit', function criarp(evento){ 
+//     evento.preventDefault()
+//         let nome = todo.querySelector('#nome').value
+//         let p = document.createElement('p')
+//         let exibindo = document.getElementById('resultado')
 
-        p.innerHTML = `Olá ${nome} você é`
+//         p.innerHTML = `Olá ${nome} você é`
 
-        exibindo.appendChild(p)
-    }) 
+//         exibindo.appendChild(p)
+//     }) 
     
--- testando/ex1/verificando.js
+++ testando/ex1/verificando.js
@@ -1,36 +1,36 @@
 
 
-// let form = document.querySelector('.dados')
+let form = document.querySelector('.dados')
 
-// form.addEventListener('submit', function bloquear(evento){
-//     evento.preventDefault()
+form.addEventListener('submit', function bloquear(evento){
+    evento.preventDefault()
 
-// })
+})
 
-// form.addEventListener('submit', function coletaDados(){
-// let nome = form.querySelector('#nome').value
-// let idade = Number(form.querySelector('#idade').value)
+form.addEventListener('submit', function coletaDados(){
+let nome = form.querySelector('#nome').value
+let idade = Number(form.querySelector('#idade').value)
 
 
-//     function verificar(){
-//             let resul;
+    function verificar(){
+            let resul;
 
-//             if(idade < 18){
-//                 resul = 'Menor de idade!'
-//             }else{
-//             resul = 'Maior de idade!'
-//             }
-//             return resul
+            if(idade < 18){
+                resul = 'Menor de idade!'
+            }else{
+            resul = 'Maior de idade!'
+            }
+            return resul
 
-//         }
+        }
         
 
-//     function criarp(){
-//         let p = document.createElement('p')
-
-//         p.innerHTML += `Olá ${nome} você é ${verificar()}`
-
-//         document.getElementById('#resulatdo').appendChild(p)
-//     }
-//     criarp()
-// })
+    function criarp(){
+        let p = document.createElement('p')
+        let exibindo = document.getElementById('resultado')
+        p.innerHTML = `Olá ${nome} você é ${verificar()}`
+        exibindo.innerHTML = " "
+        exibindo.appendChild(p)
+    }
+    criarp()
+})

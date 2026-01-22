919e955fadad993a34de9b9a63f857102fd405bf
stelsol <132797421+stelsol@users.noreply.github.com>
2026-01-22 13:50:58 -0300

    Exibindolista

---
+++ testando/ex2/exibindo.js
@@ -0,0 +1,6 @@
+// const listaN = [1,2,3,4,5]
+
+
+// for(let i = 0; i <= listaN.length; i++){
+//     console.log(i)  
+// }
---
+++ testando/ex2/teste.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('.container')
+
+form.addEventListener('click',function exibir(){
+        function adili(){
+            let li = form.querySelector('.lista')
+        console.log('foi')
+    }
+    adili()
+})

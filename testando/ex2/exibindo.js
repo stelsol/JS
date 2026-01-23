c35449b24d2e7550f4e8f0b6e3216e687c5da67c
stelsol <132797421+stelsol@users.noreply.github.com>
2026-01-23 18:40:19 -0300

    exercicio finalizado

--- testando/ex2/exibindo.js
+++ testando/ex2/exibindo.js
@@ -1,6 +1,24 @@
-// const listaN = [1,2,3,4,5]
+
+const form = document.querySelector('.container')
+
+form.addEventListener('click',function exibir(){ // criamos um evento que ao clicar ira exibir a lista de arrays!!
+        const listaN = [1,2,3,4,5]
+        let ul = document.querySelector('.lista') // pegamos a classe lista, onde sera exibida as li
 
 
-// for(let i = 0; i <= listaN.length; i++){
-//     console.log(i)  
-// }
+        if(ul.children.length === 0){
+            for(let i = 0; i < listaN.length; i++){  // laço onde será percorrido com base no comprimento do array que temos.
+                let li = document.createElement('li') // li estão sendo criadas aqui 
+                li.innerHTML = `${listaN[i]}`  // essa linha e a proxima será responsavel por exibilas
+                ul.appendChild(li)
+            }
+
+        }else{
+
+            ul.innerHTML = " " // apaga a lista toda vez que clicar no botão novamente para não criar um monte de cópias da mesma!
+
+        }
+
+    })
+    
+    
--- testando/ex2/teste.js
+++ testando/ex2/teste.js
@@ -1,9 +1,1 @@
-const form = document.querySelector('.container')
 
-form.addEventListener('click',function exibir(){
-        function adili(){
-            let li = form.querySelector('.lista')
-        console.log('foi')
-    }
-    adili()
-})

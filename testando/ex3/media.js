4c1f721bf34d834ac28591ce3bfbd646f7b72248
stelsol <132797421+stelsol@users.noreply.github.com>
2026-01-29 15:53:05 -0300

    iniciando novo exe

---
+++ testando/ex3/media.js
@@ -0,0 +1,5 @@
+// Criar um software para um profeesoar apenas disponibilizar as notas e receber: 
+// O aluno foi aprovado ou não;
+// Exibir cada nota 
+// Exibir a média final do aluno
+//! Desafio, adicione as faltas também, se ele foi ou não reprovado por falta e se for exiba isso e em cada nota a quantidade de faltas por matéria!

426320274b77e9ffaad8ac9fb9244218eb36c0b6
stelsol <132797421+stelsol@users.noreply.github.com>
2026-02-02 22:31:11 -0300

    criando vars

--- testando/ex3/media.js
+++ testando/ex3/media.js
@@ -1,5 +1,19 @@
-// Criar um software para um profeesoar apenas disponibilizar as notas e receber: 
-// O aluno foi aprovado ou não;
-// Exibir cada nota 
-// Exibir a média final do aluno
-//! Desafio, adicione as faltas também, se ele foi ou não reprovado por falta e se for exiba isso e em cada nota a quantidade de faltas por matéria!
+let doc =  document.querySelector('.container')
+let calc = doc.querySelector('.calc')
+let gerar =  doc.querySelector('.gerar')
+let nome = doc.querySelector('#nome')
+let mat = doc.querySelector('#materia')
+let nota = doc.querySelector('#nota')
+let faltas = doc.querySelector('#faltas')
+
+calc.addEventListener('click', function materia(){
+
+    let boletim = {
+        nome: nome.value, // !Retirar o nome daqui, ñ tem necessidade dele aqui toda vez!
+        mat: mat.value ,  
+        nota: Number(nota.value), 
+        faltas: Number(faltas.value)
+    }
+    console.log(boletim)
+
+})

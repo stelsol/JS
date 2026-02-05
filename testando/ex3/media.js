1c05c496e1bfb246bcc403205209b5a24cd695ed
stelsol <132797421+stelsol@users.noreply.github.com>
2026-02-04 21:02:56 -0300

    testando

--- testando/ex3/media.js
+++ testando/ex3/media.js
@@ -1,19 +1,36 @@
+
 let doc =  document.querySelector('.container')
 let calc = doc.querySelector('.calc')
 let gerar =  doc.querySelector('.gerar')
-let nome = doc.querySelector('#nome')
 let mat = doc.querySelector('#materia')
 let nota = doc.querySelector('#nota')
 let faltas = doc.querySelector('#faltas')
 
 calc.addEventListener('click', function materia(){
+    let nome = doc.querySelector('#nome').value
+    // Var para armazenar o resultado da condição
+    let situacao 
+    // Aqui eu adicionaria a condição da nota ou falta e o calculo ? 
+    if (Number(nota.value) < 6 || Number(faltas.value) > 20){
+
+        situacao = " Reprovado(a) "
+
+    }else {
+        situacao = " Aprovado(a) "
+    }
+    // e depois adicionaria o calculo e ai viria a criação do objto??
 
     let boletim = {
-        nome: nome.value, // !Retirar o nome daqui, ñ tem necessidade dele aqui toda vez!
+        // nome: nome.value, // !Retirar o nome daqui, ñ tem necessidade dele aqui toda vez!
         mat: mat.value ,  
         nota: Number(nota.value), 
-        faltas: Number(faltas.value)
+        faltas: Number(faltas.value),
+        situacao: situacao
     }
-    console.log(boletim)
-
-})
+    // console.log(nome, boletim)
+    
+    let ul = doc.querySelector('.lista')
+    let li = document.createElement('li')
+    li.innerHTML = `${nome} ${boletim.mat} ${boletim.nota} ${boletim.faltas} ${boletim.situacao}` // teste para exibir
+    ul.appendChild(li)
+}) 
